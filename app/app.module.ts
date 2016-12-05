4d7640eaf9c76ea8f8189d48549cb6e1bc740473
Todd Nichols <toddnichols@Todds-iMac-2015.local>
2016-12-04 19:50:42 -0600

    create players component and route

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
+import { PlayersComponent } from "./players/players.component";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./app.routes";
 
 @NgModule({
-    imports:      [ BrowserModule ],
-    declarations: [ AppComponent ],
+    imports:      [
+        BrowserModule,
+        RouterModule.forRoot(
+            appRoutes
+        )
+    ],
+    declarations: [
+        AppComponent,
+        PlayersComponent
+    ],
     bootstrap:    [ AppComponent ]
 })
 
---
+++ app/players/players.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'players',
+    templateUrl: 'app/players/players.html'
+})
+
+export class PlayersComponent {
+
+}
